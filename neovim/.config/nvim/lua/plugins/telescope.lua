b6dfd42e7517ce5ad2940391edb12b5aa2c0b2f8
Drew Hays <drewshays@gmail.com>
2026-04-27 21:25:58 -0700

    telescope: include untracked files in git_files picker

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -17,7 +17,7 @@ return {
     keys = {
       -- File finding
       { "<leader><leader>", function()
-          local ok = pcall(require("telescope.builtin").git_files)
+          local ok = pcall(require("telescope.builtin").git_files, { show_untracked = true })
           if not ok then
             require("telescope.builtin").find_files()
           end
